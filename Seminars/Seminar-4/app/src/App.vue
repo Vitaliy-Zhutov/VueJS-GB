43086140892411f3cfce3f7d208dca808fde437a
Vitaly <vitaly.zutov@yandex.ru>
2024-06-02 11:12:59 +0700

    Seminar-4. CLI. Vue.js

---
+++ Seminars/Seminar-4/app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Привет!</h1>
+    <HelloComponent/>
+    <FotoComponent/> 
+</template>
+
+<script>
+import HelloComponent from './components/HelloComponent.vue';
+import FotoComponent from './components/FotoComponent.vue';
+
+export default {
+  components: { HelloComponent, FotoComponent } 
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ Seminars/Seminar-4/app/src/components/FotoComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <img :src="bigImg" width="1100px">
+        <div class="catimg">
+            <img @click="selectImg(cat)" v-for="(cat, index) in cats" :key="index" :src="cat" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+import img from "@/assets/img/cat-3.jpg";
+    export default {
+        data() {
+            return {
+                bigImg: '',
+                cats: {
+                    cat1: require("@/assets/img/cat-1.jpg"),
+                    cat2: require("@/assets/img/cat-2.jpg"),
+                    cat3: img,
+                }
+            }
+        },
+        methods: {
+            selectImg(cat) {
+                this.bigImg = cat;
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
