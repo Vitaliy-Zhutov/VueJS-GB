d3510ae608e3ea4ae13ac644efbbb657e1629c30
Vitaly <vitaly.zutov@yandex.ru>
2024-06-03 23:44:41 +0700

    Homework-5. Built-in directives and working with component data. Vue.js

---
+++ Homeworks/Homework-5/task-2/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <ProductDetailsComponent/>
+</template>
+
+<script>
+import ProductDetailsComponent from './components/ProductDetailsComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductDetailsComponent,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ Homeworks/Homework-5/task-2/src/components/ProductDetailsComponent.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        Выберите валюту:
+        <select size="1" name="" id="" v-model="valuta">
+            <option>₽</option>
+            <option>€</option>
+            <option>$</option>
+        </select>
+        <br><br><br>
+        <div v-for="product in products" :key="product.id">
+            <p>Название: {{ product.name }}</p>
+            <p>Цена: {{ formattedPrice(product.price) }}</p>
+            <p v-if="product.available">Статус: Available</p>
+            <p v-else>Статус: Out of stock</p>
+            <br>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            price: '',
+            valuta: '',
+            products: [
+                {
+                    id: 1,
+                    name: 'Хлеб',
+                    price: 40,
+                    available: true
+                },
+                {
+                    id: 2,
+                    name: 'Мясо',
+                    price: 350,
+                    available: false
+                },
+                {
+                    id: 3,
+                    name: 'Молоко',
+                    price: 70,
+                    available: true
+                },
+            ]
+        };
+    },
+
+    computed: {
+        formattedPrice() {
+            if (this.valuta === '₽') {
+                return price => `₽ ${price}`
+            } else if (this.valuta === '€') {
+                return price => `€ ${(price / 96.87).toFixed(2)}`;
+            } else {
+                return price => `$ ${(price / 89.38).toFixed(2)}`
+            }
+        }
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
