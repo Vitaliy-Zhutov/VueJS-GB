0074be3edc89847462696930280816c9a3d71281
Vitaly <vitaly.zutov@yandex.ru>
2024-06-05 17:02:25 +0700

    Seminar-6.Interaction between components. Vue.js

---
+++ Seminars/Seminar-6/app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <BookListComponent/>
+  <MovieListComponent/>
+
+</template>
+
+<script>
+import BookListComponent from './components/BookListComponent.vue'
+import MovieListComponent from './components/MovieListComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BookListComponent,
+    MovieListComponent,
+  }
+}
+</script>
+
+<style lang="scss"></style>
---
+++ Seminars/Seminar-6/app/src/components/FilmComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Название: {{ film.title }} </h2>
+        <p>Год: {{ film.year }} </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            film: Object,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
