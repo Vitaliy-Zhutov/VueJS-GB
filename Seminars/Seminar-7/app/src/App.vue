4fba99e1318264c78dd84bb80042b59f1fe264c4
Vitaly <vitaly.zutov@yandex.ru>
2024-06-06 14:12:32 +0700

    Seminar-7. Application Data Management VueX. Vue.js

---
+++ Seminars/Seminar-7/app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <CounterComponent/>
+</template>
+
+<script>
+import CounterComponent from './components/CounterComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CounterComponent,
+  }
+  
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ Seminars/Seminar-7/app/src/components/CounterComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h2>Счетчик</h2>
+        <p>{{ count }}</p>
+        <button @click="increment">+</button>
+        <button @click="decrement">-</button>
+    </div>
+    <hr>
+    <h2>Корзина</h2>
+    <ul>
+        <li v-for="item in cartItems" :key="item.id">{{ item.name }} - {{ item.price }}</li>
+    </ul>
+    <p>Total: {{ totalItems }}</p>
+    <p>Total: {{ totalPrice }}</p>
+    <button @click="addItemToCart({ id: 1, name: 'Товар', price: 10})">Добавить в корзину</button>
+    <hr>
+    <h2>Данные с сервера</h2>
+    <ul>
+        <li v-for="item in data" :key="inem">{{ item }}</li>
+    </ul>
+    <button @click="fetchData">Загрузить данные</button>
+</template>
+
+<script>
+import { mapState, mapGetters, mapMutations, mapActions } from 'vuex';
+export default {
+    computed: {
+        ...mapState(['count', 'data']),
+        ...mapGetters(['cartItems', 'totalItems', 'totalPrice']),
+    },
+    methods: {
+        ...mapMutations(['increment', 'decrement']),
+        ...mapActions(['addItemToCart', 'fetchData']),
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
